d7487044de1c09aad855b0c97b961a01cd29398c
Logan Phan <logaphan@gmail.com>
2025-11-05 18:11:26 -0800

    initial implementation of the upload resume button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import React, { useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
+export default function Page(){
+  const [recievedResume, setRecievedResume] = useState<boolean>(false);
+
+  
+
+  async function uploadSubmit(e: React.FormEvent<HTMLFormElement>) {
+  setRecievedResume(true);
+}
+
+
+
+ async function startInterview(e: React.MouseEvent<HTMLButtonElement>) {
+
+
+  }
+  return(
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 
       <header className="text-4xl font-road">
@@ -15,10 +30,23 @@ export default function Home() {
           <div className="flex flex-col justify-between h-[250px]">
             
             <div className="flex items-center gap-2">
-              <span className="w-6 font-bold text-left">1.</span>
-                <button className="font-road font-bold border border-gray-50 px-4 py-2 rounded hover:bg-gray-700 cursor-pointer w-[250px]">
+              <form>
+                <div className="w-64 flex flex-col space y-4">
+                <span className="w-6 font-bold text-left"> 1.</span>
+                  <input
+                  type = "file"
+                  className="font-road font-bold border border-gray-50 px-4 py-2 rounded hover:bg-gray-700 cursor-pointer w-[250px]"
+                  name= "resume"
+                  accept = "application/pdf"
+                  required>
+                  </input>
+                <button className="font-road font-bold border border-gray-50 px-4 py-2 rounded hover:bg-gray-700 cursor-pointer w-[250px]"
+                type="submit">
                   Upload Resume
                 </button>
+                </div>
+              </form>
+              
             </div>
 
             <div className="flex items-center gap-2">
@@ -50,4 +78,8 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+  }
+
+
+
+
