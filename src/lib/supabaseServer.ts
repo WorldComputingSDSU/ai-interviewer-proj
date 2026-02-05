54b7c301a2765c052f94d298f421b968331a6e37
huaac <huaac1@uci.edu>
2026-02-05 00:11:40 -0800

    updated supabaseServer.ts

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 export function supabaseAdmin() {
     const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
     const supabaseKey = process.env.REACT_APP_ANON_KEY
-    return createClient(supabaseUrl, supabaseKey)
+    return createClient(url, key, {auth: {persistSession:false}});
+    //return createClient(supabaseUrl, supabaseKey)
 }
