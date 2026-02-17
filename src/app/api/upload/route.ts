5a3b701970c48191c1d094a3f4846ed5d634b0d9
Sqrl <68661127+Sqrl34@users.noreply.github.com>
2026-02-16 17:53:39 -0800

    fixes

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import fs from "fs";
+import { writeFile } from "fs/promises";
 import PDFParser from "pdf2json";
 
 import { chunkText } from "@/lib/chunk";
@@ -14,21 +14,23 @@ export async function POST(req: NextRequest) {
 
   //generates a session id
   let sessionId = req.cookies.get('sessionId')?.value;
+  let newSession = false;
 
   if (!sessionId) {
     sessionId = uuidv4();
+    newSession = true;
   }
 
   const formData = await req.formData();
-  const candidateFormId = formData.get("candidateId");
+  const candidateFormId = (formData.get("candidateId") as string) || "";
   const resumeFile = formData.get("resume");
   const filepond = formData.get("filepond");
-  const candidateFile = File;
+  const candidateFile = (resumeFile instanceof File ? resumeFile : filepond instanceof File ? filepond: null);
 
   let fileName = "";
   let fullText = "";
 
-  const candidateId = (candidateFormId != null) ? candidateFormId : uuidv4();
+  const candidateId = (candidateFormId && typeof candidateFormId == 'string') ? candidateFormId : uuidv4();
 
   if (candidateFile == null || candidateFile == undefined || (candidateFile instanceof File == null)) {
     return NextResponse.json(
@@ -42,7 +44,7 @@ export async function POST(req: NextRequest) {
   fileName = `${uuidv4()}.pdf`;
   const tempFilePath = `/tmp/${fileName}`;
   const fileBuffer = Buffer.from(await candidateFile.arrayBuffer());
-  await fs.writeFile(tempFilePath, fileBuffer);
+  await writeFile(tempFilePath, fileBuffer);
 
 
   const pdfParser = new PDFParser();
@@ -55,8 +57,9 @@ export async function POST(req: NextRequest) {
       const text = (pdfParser as any).getRawTextContent();
       resolve(text);
     });
+
+    pdfParser.loadPDF(tempFilePath);
   });
-  pdfParser.loadPDF(tempFilePath);
 
   //writing/embedding the chunks into supabase using openai
   try {
@@ -81,5 +84,26 @@ export async function POST(req: NextRequest) {
       console.error("Supabase insert error:", error);
       return NextResponse.json({ error: "DB insert failed" }, { status: 500 });
     }
+  } catch(e) {
+    console.error("Embedding error:", e);
+    return NextResponse.json({ error: "Embedding error" }, { status: 500 });
   }
+
+    const response = NextResponse.json({
+      parsedText,
+      fileName,
+      candidateId,
+      sessionId,
+    });
+
+    if (newSession) {
+      response.cookies.set("sessionId", sessionId, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 30,
+      })
+    }
+  return response;
 }
